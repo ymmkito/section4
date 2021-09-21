dff349147f3cfbbd8b7acd41961191406c8dfcaa
ymmkito <91085904+ymmkito@users.noreply.github.com>
2021-09-21 22:32:03 +0900

    23

--- src/index.js
+++ src/index.js
@@ -31,28 +31,47 @@ console.clear();
 // console.log(val3);
 
 //constで定義したオブジェクトのプロパティは変更可
-const val4 = {
-  name: "aaa",
-  age: 20
-};
-console.log(val4);
-val4.name = "bbb";
-console.log(val4);
-val4.addree = "Kyoto";
+// const val4 = {
+//   name: "aaa",
+//   age: 20
+// };
+// console.log(val4);
+// val4.name = "bbb";
+// console.log(val4);
+// val4.addree = "Kyoto";
 
-const val5 = ["dog", "cat"];
-console.log(val5);
-val5[0] = "bird";
-val5.push("fish");
+// const val5 = ["dog", "cat"];
+// console.log(val5);
+// val5[0] = "bird";
+// val5.push("fish");
 
-const name = "aaa";
-const age = 28;
+// const name = "aaa";
+// const age = 28;
 
-const message = "私のの名前は" + name + "です。年齢は" + age + "です。";
-console.log(message);
-const message2 = `私は名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// const message = "私のの名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message);
+// const message2 = `私は名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
 
-const sayHello = (name = "guest") => console.log(`こんにちは${name}さん`);
-sayHello("bbb");
-sayHello();
+// const sayHello = (name = "guest") => console.log(`こんにちは${name}さん`);
+// sayHello("bbb");
+// sayHello();
+
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1, num2, arr3);
+
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+const arr6 = [...arr4];
+console.log(arr6);
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
